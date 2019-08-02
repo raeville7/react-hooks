0465ab5449818a659c5112ce98badaf5dea6c775
raeville7 <raeville7@gmail.com>
2019-08-02 14:04:00 +0800

    added class component lifecycles

--- src/Traditional.js
+++ src/Traditional.js
@@ -1,18 +1,39 @@
 import React, { Component } from "react";
 
+function LifecycleDemo() {
+  return "I'm a lifecycle demo";
+}
 class Traditional extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 20
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 20,
+      mounted: true
+    };
+  }
+  componentDidMount() {
+    console.log("render!");
+  }
+  componentWillUnmount() {
+    console.log("unmounting...");
+  }
+  toggle = () => {
+    this.setState({ mounted: !this.state.mounted });
+  };
   render() {
     return (
       <div>
-        <h3>{this.props.data} UseState</h3>
+        Class Component
         <p>You clicked {this.state.count} times</p>
-        <button onClick={() => this.setState({count: this.state.count + 1})}>Click me</button>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click me
+        </button>
+        <br/>
+        <hr/>
+        <div>
+          <button onClick={this.toggle}>Show/Hide LifecycleDemo</button>
+          <div>{this.state.mounted && <LifecycleDemo />}</div>
+        </div>
       </div>
     );
   }
